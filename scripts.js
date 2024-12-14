e034d45c9ed8bf504c1c9b2cda52469b0f47351f
Nitish <nk2552003@gmail.com>
2024-12-14 13:50:30 +0530

    changes

--- scripts.js
+++ scripts.js
@@ -1,37 +1,3 @@
-const images = [
-  "url('images/1.jpg')",
-  "url('images/2.jpg')",
-  "url('images/3.jpg')",
-];
-
-let currentIndex = 0;
-
-const heroSection = document.querySelector(".hero-section");
-
-function changeBackground() {
-  // Add the fade-out effect
-  heroSection.classList.add("fade-out");
-
-  // Wait for the fade-out animation to finish (1 second), then change the background
-  setTimeout(() => {
-    currentIndex = (currentIndex + 1) % images.length;
-    heroSection.style.backgroundImage = images[currentIndex];
-
-    // Add fade-in after background change
-    heroSection.classList.remove("fade-out");
-    heroSection.classList.add("fade-in");
-
-    // Remove fade-in class after animation is done
-    setTimeout(() => {
-      heroSection.classList.remove("fade-in");
-    }, 1000); // Same duration as the CSS animation
-  }, 1000); // Same duration as the CSS animation
-}
-
-setInterval(changeBackground, 5000); // Change background every 5 seconds
-
-// navbar humburger menu
-
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 
